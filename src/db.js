129b0df8d40fe5a8dfa3f92f81853061d981dcd1
WilliamG <williamalberto1@hotmail.com>
2023-11-14 15:34:42 -0500

    Redireccionado la conexion de db a render corregido el URL

--- src/db.js
+++ src/db.js
@@ -3,7 +3,7 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST
+  DB_USER, DB_PASSWORD, DB_HOST, DB_URL
 } = process.env;
 
 //const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/videogames`, {

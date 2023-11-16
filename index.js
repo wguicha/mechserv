8ca94b855ff3ff8ac72beb662bb1ce3c4190ec57
WilliamG <williamalberto1@hotmail.com>
2023-11-16 16:39:46 -0500

    Conexion de local a bd en render

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 
 // Syncing all the models at once.
-conn.sync({ force: false }).then(() => {
+conn.sync().then(() => {
   server.listen(3001, () => {
     console.log('Server is listening'); // eslint-disable-line no-console
   });

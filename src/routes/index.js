1107f714b77c48bf45dd2ca7149f9bd906c6745a
WilliamG <williamalberto1@hotmail.com>
2023-11-21 17:02:03 -0500

    Ok filtros por catergoria y palabra clave tanto independientes o combinados

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,12 +3,14 @@ const { postUser, verifyToken } = require('../controllers/postUser');
 const getUsers = require('../controllers/getUsers');
 const login = require('../controllers/login');
 const getServices = require('../controllers/getServices');
+const searchServices = require('../controllers/searchServices');
 
 const router = Router();
 router.get('/users', getUsers);
 router.post('/users',  postUser);
 router.post('/login', login);
 router.get('/services', getServices);
+router.get('/services/search/', searchServices);
 
 
 module.exports = router;

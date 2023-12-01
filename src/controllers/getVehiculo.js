f81e3c95e6d2b724de126a5ecd6856449f7602e1
ClementeDelduca <clemente.delduca@gmail.com>
2023-12-01 11:31:03 -0400

    ruta uservehiculo

---
+++ src/controllers/getVehiculo.js
@@ -0,0 +1,36 @@
+const { User, Vehiculo } = require('../db');
+
+const getVehiculoUser = async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const user = await User.findOne({
+      where: {
+        uuid: userId,
+      },
+      include: [
+        {
+          model: Vehiculo,
+          attributes: ['marca', 'modelo', 'date'],
+        },
+      ],
+    });
+
+    if (!user) {
+      return res.status(404).json({ message: 'Usuario no encontrado' });
+    }
+
+    const vehiculos = user.Vehiculos.map((vehiculo) => {
+      return {
+        marca: vehiculo.marca,
+        modelo: vehiculo.modelo,
+        date: vehiculo.date ? (vehiculo.date.getFullYear() + 1).toString() : '', // Convierte a cadena de texto y obtiene el año + 1
+      };
+    });
+
+    return res.status(200).json({ vehiculos });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = getVehiculoUser;
